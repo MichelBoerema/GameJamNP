52738d1b01f8e3d0c3a3ee9580d726afec75300c
PenguToggled <mmaris07@live.nl>
2024-02-12 14:22:10 +0100

    Scripts

---
+++ GameJamNP2/Assets/Scripts/Door.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Animator animator;
+    public bool isOpenn = false;
+    public bool inRange = false;
+
+    void Update()
+    {
+        if (inRange)
+        {
+            if (!isOpenn && Input.GetKeyDown(KeyCode.E))
+            {
+                animator.SetBool("isOpen", true);
+                isOpenn = true;
+            }
+            else if (isOpenn && Input.GetKeyDown(KeyCode.E))
+            {
+                animator.SetBool("isOpen", false);
+                isOpenn = false;
+            }
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            inRange = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            inRange = false;
+        }
+    }
+}
