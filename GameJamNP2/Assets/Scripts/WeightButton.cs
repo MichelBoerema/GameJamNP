937da1e470ece61f08358fde0397e1a1c1682a58
Sjoerd <113519405+DreojsNL@users.noreply.github.com>
2024-02-12 13:43:24 +0100

    AddedButton

--- GameJamNP2/Assets/Scripts/WeightButton.cs
+++ GameJamNP2/Assets/Scripts/WeightButton.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events; // Import UnityEvents
 
 public class WeightButton : MonoBehaviour
 {
     public Animator animator;
     public bool Actife = false;
+
+    // UnityEvent for On and Off actions
+    public UnityEvent onActivation;
+    public UnityEvent onDeactivation;
+
     private void OnTriggerEnter(Collider other)
     {
         animator.SetBool("Active", true);
         Actife = true;
+        // Invoke the UnityEvent for activation
+        onActivation.Invoke();
     }
+
     private void OnTriggerExit(Collider other)
     {
         animator.SetBool("Active", false);
         Actife = false;
+        // Invoke the UnityEvent for deactivation
+        onDeactivation.Invoke();
     }
 }
